a79071d46d889bca25b41422515caa8fa26a836c
Aksakwo13819 <45039664+Aksakwo13819@users.noreply.github.com>
2018-11-17 14:35:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "3";
+var prefix = "5";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
